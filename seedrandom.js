6b5e7669fda2f9706561954999e009f880656b8b
yayforfood <raeweinst@gmail.com>
2016-06-03 16:29:46 -0400

    Update seedrandom.js

--- seedrandom.js
+++ seedrandom.js
@@ -18,7 +18,7 @@ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-(Math.function (pool, math) {
+(function (pool, math) {
 //
 // The following constants are related to IEEE 754 limits.
 //
